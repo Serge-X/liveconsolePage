ada3bfe53d47efb10e3f52b5bd81bcee5582a881
Serge <lionheart.sergex@gmail.com>
2020-02-18 00:07:31 -0800

    Corrected img file paths

--- src/components/aboutSite.js
+++ src/components/aboutSite.js
@@ -5,7 +5,7 @@ function About(){
 
     return(
         <div className="aboutSite" > 
-        <img src="../img/psController.jpg" className="leftPic" alt="Busted"/>
+        <img src="img/psController.jpg" className="leftPic" alt="Busted"/>
         <div className="textline" >Welcome to famous video game consoles of all time webpage! Here you
             can look forward to learning about famous consoles from different companies and learn a lot
             more about them!!</div>
